03b23fa639749af4268b7183722b7ce62c332b64
jkrieger6 <jkrieger11@gmail.com>
2023-03-24 08:28:02 -0700

    reformatted code to appear more plesant, added comments to js about functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Wrap what interacts with the DOM
 $(document).ready(function () {
-  // sets the current date and time at the top of the page.
+ //Add code to display the current date in the header of the page.
   var saveBtn = $(".saveBtn");
   var todayDate = dayjs().format("dddd, MMM DD YYYY");
   $("#currentDay").html(todayDate);
@@ -12,23 +12,8 @@ $(document).ready(function () {
   });
 });
 
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-
-
-
-// Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. 
-//Add code to display the current date in the header of the page.
+// Add code to apply the past, present, or future class to each time-
+// block by comparing the id to the current hour.
 function timeBlockColor() {
   var currentHour = dayjs().hour();
   console.log(currentHour);
@@ -42,10 +27,9 @@ function timeBlockColor() {
     } else {
       $(this).addClass("future");
     }
-
-    $("#"+hour +" textarea").val(localStorage.getItem(hour));
-
+    // Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements.
+    $("#" + hour + " textarea").val(localStorage.getItem(hour));
   });
 }
 timeBlockColor();
-
